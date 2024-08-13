d7638126b5446af58468673c68d8dc2c6fa6acf6
sauloSM2008 <122525634+sauloSM2008@users.noreply.github.com>
2024-08-13 10:06:41 +0200

    Kargerbets
    Annsj

---
+++ jst.js
@@ -0,0 +1,10 @@
+document.getElementById("sub").addEventListener("click",
+  
+  function() {
+    document.getElementById("sub1").style.display = "none"
+    document.getElementById("username").style.display = "none"
+  }
+  
+  
+  )
+    
